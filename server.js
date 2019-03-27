841e0d669917b2d2e0c1621919ba1f06bd17c51b
Jeff C <blackone787@gmail.com>
2019-03-26 21:41:20 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(8080, () => console.log('Running on port 8080'));
